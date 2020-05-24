ea720db5ec121bafe89f9884e917a48f5dce37e6
MehdiJames <44882144+MehdiJames@users.noreply.github.com>
2020-05-24 22:19:15 +0200

    correction bug

--- src/main/java/fr/uvsq/pglp/App.java
+++ src/main/java/fr/uvsq/pglp/App.java
@@ -1,8 +1,6 @@
 package fr.uvsq.pglp;
 
-import fr.uvsq.pglp.builder.composite.iterator.AnnuairePersonnels;
-import fr.uvsq.pglp.command.rpn.CalculatriceRPN;
-import fr.uvsq.pglp.dao.crud.Serialization;
+
 
 /**
  * Hello world!
@@ -13,8 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        AnnuairePersonnels.main(args);
-        CalculatriceRPN.main(args);
-        Serialization.main(args);
+        
     }
 }
